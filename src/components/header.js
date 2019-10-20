6bdf2bde95d90edeff993d5824e010f041d28423
sumit sharma <sumit@greytip.com>
2019-10-20 09:38:45 +0530

    added sass plugin and global css

--- src/components/header.js
+++ src/components/header.js
@@ -15,22 +15,40 @@ const Header = ({ siteTitle }) => (
       style={{
         margin: `0 auto`,
         height: 500,
-        maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        <Image />
-      </div> */}
-
       <div
         style={{
           display: `flex`,
-          flexDirection: `row-reverse`,
+          flexDirection: `row`,
+          justifyContent: `center`,
+          alignItems: `center`,
           marginTop: `70px`,
+          marginRight: `70px`,
         }}
       >
-        <div style={{ width: `300px` }}>
+        <div
+          style={{
+            width: `400px`,
+            textAlign: `center`,
+            margin: `10px`,
+            wordSpacing: `normal`,
+            color: `white`,
+            letterSpacing: `0`,
+          }}
+        >
+          <h1>Heyo, I'm Sumit!</h1>
+          <div className="subtext">
+            this is test textsdfsdfsdfsfsfdfsdfthis is test
+            textsdfsdfsdfsfsfdfsdfthis is test textsdfsdfsdfsfsfdfsdf
+          </div>
+          <div>this is test textsdfsdfsdfsfsfdfsdf</div>
+          <div>this is test textsdfsdfsdfsfsfdfsdf</div>
+          <div>this is test textsdfsdfsdfsfsfdfsdf</div>
+        </div>
+
+        <div style={{ width: `300px`, textAlign: `center`, margin: `10px` }}>
           <Image />
         </div>
       </div>
